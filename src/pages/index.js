37955cfe90dca8db02f66febd48d618d0cda1f8c
krlslman <selmankorall@gmail.com>
2023-03-18 00:07:12 +0300

    env caused a leak and deleted

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,22 @@ import { Product, FooterBanner, HeroBanner, Footer  } from '../../components';
 const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner />
-      {console.log("products:",products)}      
-      {console.log("bannerData:",bannerData)}      
+      {console.log("product.name : ", products)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {/* {console.log("products:",products)}      */}
+      {/* {console.log("bannerData:",bannerData[0].smallText)}      */}
       <div className="products-heading">
         <h2>Best Seller Products</h2>
         <p>speaker There are many variations passages</p>
       </div>
 
       <div className="products-container">
+        {/* {products?.map(
+          (product) => product.name )} */}
+          
         {products?.map(
-          (product) => product.name )}
+          (product) => <Product key={ product._id } product={product} /> 
+        )}
       </div>
 
       <FooterBanner />
