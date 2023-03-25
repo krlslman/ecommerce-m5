25dc94a2d497b18687c7684f6652eca9c157429a
krlslman <selmankorall@gmail.com>
2023-03-26 01:04:53 +0300

    Deploy fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,20 @@ export default function Document() {
     <Html lang="en">
       <Head >
         {/* <meta httpEquiv="Content-Security-Policy" content="style-src 'self' https://fonts.googleapis.com https://apis.google.com 'unsafe-inline'; font-src 'self' https://q.stripe.com https://fonts.gstatic.com; img-src 'self' https://cdn.sanity.io http://localhost:3000; script-src nonce=<random_value>; " /> */}
+        <link
+            href="/fonts/poppins-light.ttf"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+          <style>{`
+            @font-face {
+              font-family: 'Poppins';
+              src: url('/fonts/poppins-light.ttf');
+              font-weight: 300;
+              font-style: normal;
+            }
+          `}</style>
       </Head>
       <body>
         <Main />
